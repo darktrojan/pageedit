cb8a7c21a771c058d8bbe02dac9cf22ad315666a
Geoff Lankow <geoff@darktrojan.net>
2012-12-27 12:26:11 +1300

    Roll actions into an object

--- pageedit.js
+++ pageedit.js
@@ -2,14 +2,6 @@ var edit = {};
 
 (function() {
 var blocks = ['h1', 'h2', 'h3', 'p', 'ul', 'ol', 'div', 'li'];
-var blocksNotLists = ['h1', 'h2', 'h3', 'p'];
-
-var i = 0;
-var BOLD = i++, ITALIC = i++, UNDERLINE = i++;
-var LEFT = i++, CENTER = i++, RIGHT = i++, JUSTIFY = i++;
-var LINK = i++, U_LIST = i++, O_LIST = i++;
-var IMAGE = i++;
-var IM_FLOAT_LEFT = i++, IM_CENTER = i++, IM_FLOAT_RIGHT = i++;
 
 var CLASS_EDIT_BLOCK = 'edit_block', CLASS_SHOWN = 'edit_shown', CLASS_CURRENT = 'edit_current';
 var CLASS_SELECTED = 'edit_selected', CLASS_DISABLED = 'edit_disabled';
@@ -18,7 +10,7 @@ var CLASS_ALIGN_LEFT = 'alignleft', CLASS_ALIGN_CENTER = 'aligncenter', CLASS_AL
 var ToolbarUI = {
 	element: null,
 	nodeNameSelect: null,
-	buttons: null,
+	buttons: {},
 	chain_display: null,
 
 	init: function() {
@@ -31,40 +23,41 @@ var ToolbarUI = {
 		this.nodeNameSelect.append('option', 'Paragraph', { 'value': 'p' });
 
 		var group = this.element.append('span');
-		group.append('button#edit_bold', 'B');
-		group.append('button#edit_italic', 'I');
-		group.append('button#edit_underline', 'U');
+		this.addButton(group, 'bold', 'B');
+		this.addButton(group, 'italic', 'I');
+		this.addButton(group, 'underline', 'U');
 
 		group = this.element.append('span.edit_radio_buttons');
-		group.append('button#edit_justifyleft', 'L');
-		group.append('button#edit_justifycenter', 'C');
-		group.append('button#edit_justifyright', 'R');
-		group.append('button#edit_justifyfull', 'F');
+		this.addButton(group, 'justifyleft', 'L');
+		this.addButton(group, 'justifycenter', 'C');
+		this.addButton(group, 'justifyright', 'R');
+		this.addButton(group, 'justifyfull', 'F');
 
 		group = this.element.append('span');
-		group.append('button#edit_link', 'a');
+		this.addButton(group, 'link', 'a');
 
 		group = this.element.append('span.edit_radio_buttons');
-		group.append('button#edit_ulist', '\u2022');
-		group.append('button#edit_olist', '#');
+		this.addButton(group, 'ulist', '\u2022');
+		this.addButton(group, 'olist', '#');
 
 		group = this.element.append('span');
-		group.append('button#edit_image', 'im');
+		this.addButton(group, 'image', 'im');
 
 		group = this.element.append('span.edit_radio_buttons');
-		group.append('button#edit_im_alignleft', 'L');
-		group.append('button#edit_im_aligncenter', 'C');
-		group.append('button#edit_im_alignright', 'R');
+		this.addButton(group, 'im_alignleft', 'L');
+		this.addButton(group, 'im_aligncenter', 'C');
+		this.addButton(group, 'im_alignright', 'R');
 
 		this.chain_display = this.element.append('div#edit_chain');
 
 		this.element.addEventListener('change', function(aEvent) {
 			if (aEvent.target.id == 'edit_node_name') {
-				nodeNameAction();
+				Actions.nodeNameAction();
 			}
 		}, false);
 		this.element.addEventListener('click', function(aEvent) {
-			switch (aEvent.target.id) {
+			var id = aEvent.target.id;
+			switch (id) {
 			case 'edit_bold':
 			case 'edit_italic':
 			case 'edit_underline':
@@ -72,34 +65,34 @@ var ToolbarUI = {
 			case 'edit_justifycenter':
 			case 'edit_justifyright':
 			case 'edit_justifyfull':
-				return action(aEvent.target.id.substring(5), null);
+				return Actions.action(id.substring(5), null);
 			case 'edit_link':
-				return linkAction();
+				return Actions.linkAction();
 			case 'edit_ulist':
-				return listAction(U_LIST);
 			case 'edit_olist':
-				return listAction(O_LIST);
+				return Actions.listAction(id.substr(5, 2));
 			case 'edit_image':
-				return imageAction();
+				return Actions.imageAction();
 			case 'edit_im_alignleft':
 			case 'edit_im_aligncenter':
 			case 'edit_im_alignright':
-				return imageAlignAction(aEvent.target.id.substring(8));
+				return Actions.imageAlignAction(id.substring(8));
 			}
 		}, false);
 
 		this.nodeNameSelect.onmousedown = saveSelection;
-		this.buttons = ToolbarUI.element.querySelectorAll('button');
-		for (var i = 0; i < this.buttons.length; i++) {
-			this.buttons[i].onmousedown = saveSelection;
-			this.buttons[i].onmouseup = restoreSelection;
-		}
 	},
 	show: function() {
 		this.element.classList.add(CLASS_SHOWN);
 	},
 	hide: function() {
 		this.element.classList.remove(CLASS_SHOWN);
+	},
+	addButton: function(aGroup, aName, aText) {
+		var button = aGroup.append('button#edit_' + aName, aText);
+		button.onmousedown = saveSelection;
+		button.onmouseup = restoreSelection;
+		this.buttons[aName] = button;
 	},
 	setNodeName: function(aNodeName) {
 		this.nodeNameSelect.selectedIndex = -1;
@@ -123,6 +116,183 @@ var ToolbarUI = {
 	}
 };
 ToolbarUI.init();
+
+var Actions = {
+	action: function(aCommand, aValue) {
+		if (!currentDiv) return;
+		document.execCommand(aCommand, false, aValue);
+	},
+	nodeNameAction: function() {
+		var blockNode = getBlockNodeForSelection();
+		if (!blockNode)
+			return;
+		if ('selectNodeContents' in range)
+			range.selectNodeContents(blockNode);
+		else
+			range.moveToElementText(blockNode);
+		this.action('formatblock', '<' + ToolbarUI.nodeNameSelect.value + '>');
+		currentDiv.focus();
+		updateUI();
+	},
+	linkAction: function() {
+		var button = ToolbarUI.buttons[LINK];
+		if (button.classList.contains(CLASS_SELECTED)) {
+			var node = range.startContainer;
+			if (node.nodeType == 3 && range.startOffset == node.length && node.nextSibling) {
+				node = node.nextSibling;
+			}
+			if (node.childNodes.length == 1) {
+				node = node.firstChild;
+			}
+			if (node.nodeType == 1) {
+				node = node.childNodes[range.startOffset];
+			}
+			while (node) {
+				if (node.nodeType == 1 && node.localName == 'a')
+					break;
+				node = node.parentNode;
+			}
+			if ('selectNode' in range)
+				range.selectNode(node);
+			else
+				range.moveToElementText(node);
+			this.action('unlink', null);
+		} else {
+			var href;
+			if (typeof edit.linkCallback == 'function')
+				href = edit.linkCallback('text' in range ? range.text : range.toString(),
+					function(aHref) {
+						restoreSelection();
+						Actions.action('createlink', aHref);
+					});
+			else
+				href = prompt('Type or paste a link:');
+			if (href)
+				this.action('createlink', href);
+		}
+	},
+	listAction: function(aListType) {
+		var currentListType = null;
+		if (ToolbarUI.buttons.ulist.classList.contains(CLASS_SELECTED))
+			currentListType = 'ul';
+		else if (ToolbarUI.buttons.olist.classList.contains(CLASS_SELECTED))
+			currentListType = 'ol';
+
+		var blockNode = getBlockNodeForSelection();
+		if (!blockNode)
+			return;
+
+		if (!currentListType) {
+			this.action(aListType == 'ul' ? 'insertUnorderedList' : 'insertOrderedList', null);
+			updateUI();
+			return;
+		}
+
+		var list;
+		if ('startContainer' in range) {
+			list = range.startContainer;
+			if (list.nodeType == 1)
+				list = list.childNodes[range.startOffset];
+		} else {
+			var r2 = range.duplicate();
+			list = r2.parentElement();
+		}
+		while (list) {
+			if (list.nodeType == 1) {
+				if (list.localName == 'ul' || list.localName == 'ol') {
+					blockNode = list;
+					break;
+				}
+				if (list.classList.contains(CLASS_EDIT_BLOCK)) {
+					break;
+				}
+			}
+			list = list.parentNode;
+		}
+		var parent = blockNode.parentNode;
+
+		if (currentListType == aListType) { // already a list, remove
+			var next = blockNode.nextSibling;
+			range.setStartAfter(blockNode);
+			for (var i = 0; i < blockNode.childElementCount; i++) {
+				var li = blockNode.children[i];
+				var p = document.createElement('p');
+				var n;
+				while (n = li.firstChild) {
+					if ((!parent.classList.contains(CLASS_EDIT_BLOCK)) ||
+							(n.nodeType == 1 && blocks.indexOf(n.localName) >= 0)) {
+						parent.insertBefore(n, next);
+						range.setEndAfter(n);
+					} else {
+						p.appendChild(n);
+					}
+				}
+				if (p.childNodes.length) {
+					parent.insertBefore(p, next);
+					range.setEndAfter(p);
+				}
+			}
+			parent.removeChild(blockNode);
+
+		} else { // convert to other type of list
+			var newList = document.createElement(aListType);
+			var li;
+			while (li = blockNode.firstChild) {
+				newList.appendChild(li);
+			}
+			parent.replaceChild(newList, blockNode);
+			if ('selectNode' in range)
+				range.selectNode(newList);
+			else
+				range.moveToElementText(newList);
+		}
+		updateUI();
+	},
+	imageAction: function() {
+		function callbackCallback(aHref) {
+			var block = getBlockNodeForSelection();
+			var newBlock = document.createElement('div');
+			var image = document.createElement('img');
+			image.setAttribute('src', aHref);
+			newBlock.appendChild(image);
+			block.parentNode.insertBefore(newBlock, block);
+		}
+		var href;
+		if (typeof edit.imageCallback == 'function')
+			href = edit.imageCallback(function(aHref) {
+				restoreSelection();
+				callbackCallback(aHref);
+			});
+		else
+			href = 'chiefs.png';
+		if (href) {
+			callbackCallback(href);
+		}
+	},
+	imageAlignAction: function(aClassName) {
+		var node;
+		if ('getSelection' in window) {
+			var a = window.getSelection();
+			if (!a.rangeCount)
+				return;
+			var r = a.getRangeAt(0);
+			node = r.startContainer;
+			if (node.nodeType == 1)
+				node = node.childNodes[r.startOffset];
+			else if (node.nodeType == 3 && r.startOffset == node.length && node.nextSibling)
+				node = node.nextSibling;
+			else if (node.childNodes.length == 1)
+				node = node.firstChild;
+		}
+		if (!node || node.localName != 'img')
+			return;
+
+		node.classList.remove(CLASS_ALIGN_LEFT);
+		node.classList.remove(CLASS_ALIGN_CENTER);
+		node.classList.remove(CLASS_ALIGN_RIGHT);
+		node.classList.add(aClassName);
+	}
+};
 
 var range;
 var currentDiv = null;
@@ -282,202 +452,22 @@ function updateUI() {
 		bold, italic, underline, link
 	);
 
-	ToolbarUI.setButtonState(BOLD, bold, collapsed);
-	ToolbarUI.setButtonState(ITALIC, italic, collapsed);
-	ToolbarUI.setButtonState(UNDERLINE, underline, collapsed);
-	ToolbarUI.setButtonState(LEFT, alignment == 'left', image);
-	ToolbarUI.setButtonState(CENTER, alignment == 'center', image);
-	ToolbarUI.setButtonState(RIGHT, alignment == 'right', image);
-	ToolbarUI.setButtonState(JUSTIFY, alignment == 'justify', image);
-	ToolbarUI.setButtonState(LINK, link, (image || collapsed) && !link);
-	ToolbarUI.setButtonState(U_LIST, uList, image);
-	ToolbarUI.setButtonState(O_LIST, oList, image);
-	ToolbarUI.setButtonState(IMAGE, false, image);
-	ToolbarUI.setButtonState(IM_FLOAT_LEFT, image && leafNode.classList.contains(CLASS_ALIGN_LEFT), !image);
-	ToolbarUI.setButtonState(IM_CENTER, image && leafNode.classList.contains(CLASS_ALIGN_CENTER), !image);
-	ToolbarUI.setButtonState(IM_FLOAT_RIGHT, image && leafNode.classList.contains(CLASS_ALIGN_RIGHT), !image);
+	ToolbarUI.setButtonState('bold', bold, collapsed);
+	ToolbarUI.setButtonState('italic', italic, collapsed);
+	ToolbarUI.setButtonState('underline', underline, collapsed);
+	ToolbarUI.setButtonState('justifyleft', alignment == 'left', image);
+	ToolbarUI.setButtonState('justifycenter', alignment == 'center', image);
+	ToolbarUI.setButtonState('justifyright', alignment == 'right', image);
+	ToolbarUI.setButtonState('justifyfull', alignment == 'justify', image);
+	ToolbarUI.setButtonState('link', link, (image || collapsed) && !link);
+	ToolbarUI.setButtonState('ulist', uList, image);
+	ToolbarUI.setButtonState('olist', oList, image);
+	ToolbarUI.setButtonState('image', false, image);
+	ToolbarUI.setButtonState('im_alignleft', image && leafNode.classList.contains(CLASS_ALIGN_LEFT), !image);
+	ToolbarUI.setButtonState('im_aligncenter', image && leafNode.classList.contains(CLASS_ALIGN_CENTER), !image);
+	ToolbarUI.setButtonState('im_alignright', image && leafNode.classList.contains(CLASS_ALIGN_RIGHT), !image);
 
 	ToolbarUI.setNodeName(blockNode.localName);
-}
-
-function action(command, value) {
-	if (!currentDiv) return;
-	document.execCommand(command, false, value);
-}
-
-function nodeNameAction() {
-	var blockNode = getBlockNodeForSelection();
-	if (!blockNode)
-		return;
-	if ('selectNodeContents' in range)
-		range.selectNodeContents(blockNode);
-	else
-		range.moveToElementText(blockNode);
-	action('formatblock', '<' + ToolbarUI.nodeNameSelect.value + '>');
-	currentDiv.focus();
-	updateUI();
-}
-
-function linkAction() {
-	var button = ToolbarUI.buttons[LINK];
-	if (button.classList.contains(CLASS_SELECTED)) {
-		var node = range.startContainer;
-		if (node.nodeType == 3 && range.startOffset == node.length && node.nextSibling) {
-			node = node.nextSibling;
-		}
-		if (node.childNodes.length == 1) {
-			node = node.firstChild;
-		}
-		if (node.nodeType == 1) {
-			node = node.childNodes[range.startOffset];
-		}
-		while (node) {
-			if (node.nodeType == 1 && node.localName == 'a')
-				break;
-			node = node.parentNode;
-		}
-		if ('selectNode' in range)
-			range.selectNode(node);
-		else
-			range.moveToElementText(node);
-		action('unlink', null);
-	} else {
-		var href;
-		if (typeof edit.linkCallback == 'function')
-			href = edit.linkCallback('text' in range ? range.text : range.toString(),
-				function(aHref) {
-					restoreSelection();
-					action('createlink', aHref);
-				});
-		else
-			href = prompt('Type or paste a link:');
-		if (href)
-			action('createlink', href);
-	}
-}
-
-function listAction(listType) {
-	var currentListType = null;
-	if (ToolbarUI.buttons[U_LIST].classList.contains(CLASS_SELECTED))
-		currentListType = U_LIST;
-	else if (ToolbarUI.buttons[O_LIST].classList.contains(CLASS_SELECTED))
-		currentListType = O_LIST;
-
-	var blockNode = getBlockNodeForSelection();
-	if (!blockNode)
-		return;
-
-	if (!currentListType) {
-		action(listType == U_LIST ? 'insertUnorderedList' : 'insertOrderedList', null);
-		updateUI();
-		return;
-	}
-
-	var list;
-	if ('startContainer' in range) {
-		list = range.startContainer;
-		if (list.nodeType == 1)
-			list = list.childNodes[range.startOffset];
-	} else {
-		var r2 = range.duplicate();
-		list = r2.parentElement();
-	}
-	while (list) {
-		if (list.nodeType == 1) {
-			if (list.localName == 'ul' || list.localName == 'ol') {
-				blockNode = list;
-				break;
-			}
-			if (list.classList.contains(CLASS_EDIT_BLOCK)) {
-				break;
-			}
-		}
-		list = list.parentNode;
-	}
-	var parent = blockNode.parentNode;
-
-	if (currentListType == listType) { // already a list, remove
-		var next = blockNode.nextSibling;
-		range.setStartAfter(blockNode);
-		for (var i = 0; i < blockNode.childElementCount; i++) {
-			var li = blockNode.children[i];
-			var p = document.createElement('p');
-			var n;
-			while (n = li.firstChild) {
-				if ((!parent.classList.contains(CLASS_EDIT_BLOCK)) ||
-						(n.nodeType == 1 && blocks.indexOf(n.localName) >= 0)) {
-					parent.insertBefore(n, next);
-					range.setEndAfter(n);
-				} else {
-					p.appendChild(n);
-				}
-			}
-			if (p.childNodes.length) {
-				parent.insertBefore(p, next);
-				range.setEndAfter(p);
-			}
-		}
-		parent.removeChild(blockNode);
-
-	} else { // convert to other type of list
-		var newList = document.createElement(listType == U_LIST ? 'ul' : 'ol');
-		var li;
-		while (li = blockNode.firstChild) {
-			newList.appendChild(li);
-		}
-		parent.replaceChild(newList, blockNode);
-		if ('selectNode' in range)
-			range.selectNode(newList);
-		else
-			range.moveToElementText(newList);
-	}
-	updateUI();
-}
-
-function imageAction() {
-	function callbackCallback(aHref) {
-		var block = getBlockNodeForSelection();
-		var newBlock = document.createElement('div');
-		var image = document.createElement('img');
-		image.setAttribute('src', aHref);
-		newBlock.appendChild(image);
-		block.parentNode.insertBefore(newBlock, block);
-	}
-	var href;
-	if (typeof edit.imageCallback == 'function')
-		href = edit.imageCallback(function(aHref) {
-			restoreSelection();
-			callbackCallback(aHref);
-		});
-	else
-		href = 'chiefs.png';
-	if (href) {
-		callbackCallback(href);
-	}
-}
-
-function imageAlignAction(className) {
-	var node;
-	if ('getSelection' in window) {
-		var a = window.getSelection();
-		if (!a.rangeCount)
-			return;
-		var r = a.getRangeAt(0);
-		node = r.startContainer;
-		if (node.nodeType == 1)
-			node = node.childNodes[r.startOffset];
-		else if (node.nodeType == 3 && r.startOffset == node.length && node.nextSibling)
-			node = node.nextSibling;
-		else if (node.childNodes.length == 1)
-			node = node.firstChild;
-	}
-	if (!node || node.localName != 'img')
-		return;
-
-	node.classList.remove(CLASS_ALIGN_LEFT);
-	node.classList.remove(CLASS_ALIGN_CENTER);
-	node.classList.remove(CLASS_ALIGN_RIGHT);
-	node.classList.add(className);
 }
 
 function getBlockNodeForSelection() {
